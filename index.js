a92f636b50fbe12afa237811df3f943937312dc6
akshatrajput009 <121870145+akshatrajput009@users.noreply.github.com>
2023-08-09 13:52:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const renderCards = function (obj, index) {
 </div>`;
 };
 
-fetch(url,"https://github.com/akshatrajput009/gaanaClone/blob/main/ganna.json")
+fetch("https://github.com/akshatrajput009/gaanaClone/blob/main/ganna.json")
   .then((res) => res.json())
   .then((data) => {
     currentPlaylist = Object.values(data)[0];
